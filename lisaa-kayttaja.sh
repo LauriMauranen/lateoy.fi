ca62b58d36b09e42fef30d3e03f29ecf466a2a3f
Lauri Mauranen <lauri.mauranen@gmail.com>
2026-01-02 13:36:21 +0200

    lisaa-kayttaja.sh

---
+++ lisaa-kayttaja.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ "$#" == 0 ]]; then
+	echo "Anna ainakin yksi käyttäjä"
+	exit 1
+fi
+
+if [[ $@ =~ (^|\ )- ]]; then
+	echo "Komento ei ota vipuja!"
+	exit 1
+fi
+
+data=/www-data/
+
+for kayttaja in "$@"; do
+	if [[ -z "$kayttaja" ]]; then
+		echo "Käyttäjänimi puuttuu, ohitetaan"
+		continue
+	fi
+
+	useradd -s /bin/bash -U -m -G users "$kayttaja" || exit 2
+
+	echo "Luotiin käyttäjä $kayttaja"
+	
+	home="/home/$kayttaja/"
+
+	mkdir "$home/.ssh"
+	touch "$home/.ssh/authorized_keys"
+
+	chown "$kayttaja:$kayttaja" "$home/.ssh" -R
+
+	mkdir "$data/$kayttaja"
+
+	chown "$kayttaja:nginx" "$data/$kayttaja"
+
+	echo "Luotiin tarvittavat kansiot käyttäjälle $kayttaja"
+done
