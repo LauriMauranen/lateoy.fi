ae7411dd8bcc2f478c76b097eb603f812fd2945f
Lauri Mauranen <lauri.mauranen@gmail.com>
2026-01-29 17:42:34 +0200

    Lisätään domain linodeen

--- lisaa-domain.sh
+++ lisaa-domain.sh
@@ -75,6 +75,36 @@ chown lauri:lauri "$nginx_conf"
 
 echo "Luotiin $nginx_conf"
 
+# linode
+
+cli_token=$(cat /home/lauri/.secrets/linode/cli.token)
+ip=172.234.123.168 
+email=lauri.mauranen@gmail.com
+
+domain_id=
+linode_domain=$domain
+if [[ "$domain" =~ lateoy\.fi$ ]]; then
+    domain_id=3388999
+    linode_domain=${domain%.*} 
+fi
+
+if [[ -z "$domain_id" ]]; then
+    tulos=$(podman compose run --rm -e LINODE_CLI_TOKEN=$cli_token linode-cli \
+	domains create --name $linode_domain --type master --soa_email $email)
+
+    [[ $tulos =~ [0-9]+ ]] && domain_id="${BASH_REMATCH[0]}"
+fi
+
+if [[ -z $domain_id ]]; then 
+    echo "Domainin luonnissa tapahtui virhe!"
+    exit 1
+fi
+
+podman compose run --rm -e LINODE_CLI_TOKEN=$CLI_TOKEN linode-cli \
+    domains records-create --name $linode_domain --type A --target $IP $domain_id
+
+# päivitetään nginx
+
 if podman exec nginx nginx -t; then
 	podman exec nginx nginx -s reload
 	echo "Ladattiin uusi nginx-konfiguraatio. https://$domain toimii nyt."
